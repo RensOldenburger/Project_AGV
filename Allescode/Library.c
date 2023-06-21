5030252bcec16c5fa89a780c045d83099bc086c7
AldarvdWeck <aldarvanderweck@gmail.com>
2023-06-21 09:35:34 +0200

    gt

---
+++ Allescode/Library.c
@@ -0,0 +1,116 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdlib.h>
+#include <avr/interrupt.h>
+#include "Library.h"
+
+volatile int timerteller = 0;
+volatile int timertellerbak = 0;
+volatile int bakvar = 0;
+
+void bochtrechts()
+{
+    PORT &= ~(1 << PIN11);
+    PORT |= (1 << PIN12);
+    PORT &= ~(1 << PIN21);
+    PORT |= (1 << PIN22);
+}
+
+void rechtdoorrijden()
+{
+    PORT &= ~(1 << PIN11);
+    PORT |= (1 << PIN12);
+    PORT |= (1 << PIN21);
+    PORT &= ~(1 << PIN22);
+}
+
+void achteruitrijden()
+{
+    PORT |= (1 << PIN11);
+    PORT &= ~(1 << PIN12);
+    PORT &= ~(1 << PIN21);
+    PORT |= (1 << PIN22);
+}
+
+void naarrechtsrijden()
+{
+    PORT &= ~(1 << PIN12);
+    PORT |= (1 << PIN11);
+    PORT &= ~(1 << PIN22);
+    PORT |= (1 << PIN21);
+}
+
+void naarlinkssrijden()
+{
+    PORT |= (1 << PIN12);
+    PORT &= ~(1 << PIN11);
+    PORT |= (1 << PIN22);
+    PORT &= ~(1 << PIN21);
+}
+
+void signaal_geven()
+{
+    PORT_LED |= (1<<LED_1);         // LED 1 aan
+    PORT_LED &= ~(1<<LED_2);        // LED 2 uit
+    PORT_buzzer |= (1<<buzzer);     // buzzer aan
+    _delay_ms(500);
+    PORT_LED |= (1<<LED_2);         // LED 2 aan
+    PORT_LED &= ~(1<<LED_1);        // LED 1 uit
+    PORT_buzzer &= ~(1<<buzzer);    // buzzer uit
+}
+
+void h_bridgeR_set_percentage(signed char percentage)
+{
+	if (percentage >= 0 && percentage <= 100)
+	{
+			OCR1A = (65536*percentage)/100;
+	}
+}
+
+void h_bridgeL_set_percentage(signed char percentage)
+{
+	if (percentage >= 0 && percentage <= 100)
+	{
+			OCR3A = (65536*percentage)/100;
+	}
+}
+
+//H-brug PWMR
+ISR(TIMER1_OVF_vect)
+{
+    PORT ^= (1 << PEN1);
+}
+//H-brug PWMR
+ISR(TIMER1_COMPA_vect)
+{
+    PORT |= (1 << PEN1);
+}
+
+//H-brug PWML
+ISR(TIMER3_OVF_vect)
+{
+    PORT ^= (1 << PEN2);
+}
+//H-brug PWML
+ISR(TIMER3_COMPA_vect)
+{
+    PORT |= (1 << PEN2);
+}
+
+//Timer Noodtoestand
+ISR(TIMER0_OVF_vect)
+{
+    timerteller ++;
+}
+
+//Timer Bak detectie
+ISR(TIMER4_OVF_vect)
+{
+    if(timertellerbak >= 2)
+    {
+        timertellerbak = 0;
+        bakvar = 0;
+        TCCR4B = 0;
+    }
+    timertellerbak ++;
+}
